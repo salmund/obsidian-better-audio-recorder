88a294e994af2bd760c6a789dbf5916c4f882a76
salmund <105465034+salmund@users.noreply.github.com>
2022-05-14 01:28:37 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ import {
 	PluginSettingTab,
 	Setting,
 } from "obsidian";
-
+const { clipboard } = require("electron");
 import moment from "moment";
 
 interface BetterAudioRecorderSettings {
@@ -25,24 +25,16 @@ export default class BetterAudioRecorderPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-		this.app.vault.create();
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
+			"microphone",
+			"Audio Recorder Interface",
 			(evt: MouseEvent) => {
 				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
+				new AudioRecordModal(this.app).open();
 			}
 		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: "BAU-open-modal",
 			name: "Open Audio Recorder Interface",
@@ -51,49 +43,9 @@ export default class BetterAudioRecorderPlugin extends Plugin {
 				new AudioRecordModal(this.app).open();
 			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new AudioRecordModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new BetterAudioRecorderSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
+		// this.addSettingTab(new BetterAudioRecorderSettingTab(this.app, this));
 	}
 
 	onunload() {}
@@ -171,7 +123,13 @@ class AudioRecordModal extends Modal {
 		recordButton.addEventListener("click", startRecording);
 		stopButton.addEventListener("click", stopRecording);
 		pauseButton.addEventListener("click", pauseRecording);
+
+		var save_button = contentEl.createEl("button", { text: "Save" });
+		MainDiv.appendChild(save_button);
+		save_button.style.display = "none";
+
 		function startRecording() {
+			save_button.style.display = "none";
 			new Notice("Recording started !");
 			console.log("recordButton clicked");
 
@@ -294,16 +252,22 @@ class AudioRecordModal extends Modal {
 			gumStream.getAudioTracks()[0].stop();
 		}
 		async function createDownloadLink(blob: any) {
+			save_button.style.display = "inline";
 			var now = moment().format("YYYYMMwebmDDHHmm");
-			let filename = `Recording-${now}.${extension}`;
+			let recording_filename = `Recording-${now}.${extension}`;
 			var url = URL.createObjectURL(blob);
 
-			blob.arrayBuffer().then(data => {
-				let blobdata = data;
-				console.log(blobdata);
-				app.vault.createBinary(`/${filename}`, blobdata);
-			  })
-			  
+			save_button.addEventListener("click", () => {
+				blob.arrayBuffer().then((data) => {
+					let blobdata = data;
+					console.log(blobdata);
+					app.vault.createBinary(`/${recording_filename}`, blobdata);
+					new Notice(
+						`${recording_filename} saved ! Link copied to clipboard`
+					);
+					clipboard.writeText(`![[${recording_filename}]]`);
+				});
+			});
 
 			// let audio_tag = contentEl.createEl("audio"); -- décommenter permet de conserver les traces des audios
 			audio_tag.setAttribute("controls", "true");
@@ -317,33 +281,33 @@ class AudioRecordModal extends Modal {
 	}
 }
 
-class BetterAudioRecorderSettingTab extends PluginSettingTab {
-	plugin: BetterAudioRecorderPlugin;
+// class BetterAudioRecorderSettingTab extends PluginSettingTab {
+// 	plugin: BetterAudioRecorderPlugin;
 
-	constructor(app: App, plugin: BetterAudioRecorderPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+// 	constructor(app: App, plugin: BetterAudioRecorderPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
 
-	display(): void {
-		const { containerEl } = this;
+// 	display(): void {
+// 		const { containerEl } = this;
 
-		containerEl.empty();
+// 		containerEl.empty();
 
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
+// 		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
-}
+// 		new Setting(containerEl)
+// 			.setName("Setting #1")
+// 			.setDesc("It's a secret")
+// 			.addText((text) =>
+// 				text
+// 					.setPlaceholder("Enter your secret")
+// 					.setValue(this.plugin.settings.mySetting)
+// 					.onChange(async (value) => {
+// 						console.log("Secret: " + value);
+// 						this.plugin.settings.mySetting = value;
+// 						await this.plugin.saveSettings();
+// 					})
+// 			);
+// 	}
+// }
